8b76d8dc006644e2582642a29ee03739873e184b
Milad Asadi <46265665+Milad-PC@users.noreply.github.com>
2025-09-02 15:25:51 +0330

    DataBase Options is Set

---
+++ DataLayer/Context/MyContext.cs
@@ -0,0 +1,35 @@
+﻿using DataLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Context
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AdminUser>().HasData(
+                new AdminUser()
+                {
+                    AdminUserId = 1,
+                    FullName = "میلاد اسدی",
+                    UserName = "lightco",
+                    Password = "123456789",
+                    Phone = "09116430304",
+                    RegisterDate = new DateTime(2025, 09, 01),
+                    UserImage = "Default.png",
+                    IsActive = true
+                }
+                );
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<AdminUser> Users { get; set; }
+        public DbSet<Place> Places { get; set; }
+        public DbSet<Property> Properties { get; set; }
+    }
+}
--- OtaghMazandaran/Program.cs
+++ OtaghMazandaran/Program.cs
@@ -1,7 +1,29 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using DataLayer.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//Auth
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(option =>
+    {
+        option.LoginPath = "/admin/login";
+        option.LogoutPath = "/";
+        option.ExpireTimeSpan = TimeSpan.FromDays(31);
+    });
+//Local DataBase
+builder.Services.AddDbContext<MyContext>(dboption =>
+    dboption.UseSqlServer("Server=.;Database=PRS_Db;Integrated Security=true;TrustServerCertificate=True"));
+//Remote Database
+//builder.Services.AddDbContext<MyContext>(options =>
+//    options.UseSqlServer("Server=myserver;Database=mydb;User Id=myUsr;Password=mypass;MultipleActiveResultSets=true;TrustServerCertificate=True;Encrypt=True;")
+//);
+
+
+//builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var app = builder.Build();
 
