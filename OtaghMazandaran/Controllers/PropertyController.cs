aefb50eb1503310ecf1dbc9e35bf76cdc0b8b7fa
Milad Asadi <46265665+Milad-PC@users.noreply.github.com>
2025-09-22 16:49:59 +0330

    start create property

--- OtaghMazandaran/Controllers/PropertyController.cs
+++ OtaghMazandaran/Controllers/PropertyController.cs
@@ -1,19 +1,39 @@
-﻿using DataLayer.Repositories;
+﻿using DataLayer.DTOs;
+using DataLayer.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace OtaghMazandaran.Controllers
 {
     public class PropertyController : Controller
     {
         private IPropertyRepository _db;
-        public PropertyController(IPropertyRepository propertyRepository)
+        private IPlaceRepository _place;
+        public PropertyController(IPropertyRepository propertyRepository,IPlaceRepository placeRepository)
         {
             _db = propertyRepository;
+            _place = placeRepository;
         }
         public IActionResult Index(int id = 0)
         {
             if(id == 0) return View(_db.GetAll());
             else return View(_db.GetAll(id));
         }
+        public IActionResult Create()
+        {
+            ViewBag.Places = _place.GetAll().Select(u => new SelectListItem
+            {
+                Value = u.PlaceId.ToString(),
+                Text = u.PlaceName
+            }).ToList() as List<SelectListItem>;
+
+            return View();
+        }
+        public IActionResult Create([Bind(include: "PropertyName,PropertyBrand,PropertyColor,PropertyDescription,PlaceId")]CreatePropertyMV src)
+        {
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit() { return View(); }
+        public IActionResult Delete() { return View(); }
     }
 }
