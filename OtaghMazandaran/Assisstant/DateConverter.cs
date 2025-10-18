65f29422b3334af5951edf495aea30340048ed25
Milad Asadi <46265665+Milad-PC@users.noreply.github.com>
2025-10-18 14:59:01 +0330

    name vallahe ke

---
+++ OtaghMazandaran/Assisstant/DateConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace OtaghMazandaran
+{
+    public static class DateConverter
+    {
+        public static string ToShamsi(this DateTime val)
+        {
+            PersianCalendar pc = new PersianCalendar();
+            return string.Format("{0:0000}/{1:00}/{2:00} {3:00}:{4:00}:{5:00}", pc.GetYear(val),
+                pc.GetMonth(val), pc.GetDayOfMonth(val), val.Hour, val.Minute, val.Second);
+        }
+        public static string ToShamsiString(this DateTime val)
+        {
+            string[] month = { "فروردین", "اردیبهشت", "خرداد", "تیر", "مرداد", "شهریور", "مهر", "آبان", "آذر", "دی", "بهمن", "اسفند" };
+            PersianCalendar pc = new PersianCalendar();
+            return string.Format("{0:0000} {1} {2:00}", pc.GetYear(val),
+                month[pc.GetMonth(val) - 1], pc.GetDayOfMonth(val));
+        }
+    }
+}
